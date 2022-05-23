b9bd5fe889025afde312a756e4c4a6033ff4bdb2
CharlesLiu <charles.liu@upblocks.io>
2022-05-23 14:59:38 +0800

    2. Literal(machine) : add impl machine loop pc

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,37 @@ enum Literal {
     Str(&'static str),
 }
 
+#[derive(Debug, Clone, Copy)]
+enum Instruction {}
+
+struct Machine {
+    stack: Vec<Literal>,
+    code: Vec<Instruction>,
+    pc: usize,
+}
+
+impl Machine {
+    fn new(code: Vec<Instruction>) -> Machine {
+        Machine {
+            stack: Vec::new(),
+            code,
+            pc: 0,
+        }
+    }
+
+    fn execute(&mut self) {
+        while self.pc < self.code.len() {
+            let opcode = self.code[self.pc];
+            self.pc += 1;
+            self.dispatch(opcode);
+        }
+    }
+
+    fn dispatch(&mut self, opcode: Instruction) {
+        match opcode {
+            _ => todo!(),
+        };
+    }
+}
+
 fn main() {}
